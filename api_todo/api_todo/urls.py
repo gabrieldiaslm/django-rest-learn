e28424a0c26dbf25ecb478e27037b2e01ed7e327
gabrieldiaslm <xgabrieldias777@gmail.com>
2026-02-05 11:20:53 -0300

    pequeno ajuste na url inicial

--- api_todo/api_todo/urls.py
+++ api_todo/api_todo/urls.py
@@ -22,7 +22,7 @@ from api.views import dashboard_view, calendar_view, records_list_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls') ),
-    path('dashboard/', dashboard_view, name='dashboard'),
+    path('', dashboard_view, name='dashboard'),
     path('calendar/', calendar_view, name='calendar'),
     path('records/', records_list_view, name='records_list'),
     
